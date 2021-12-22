9667a3babb8d4783735f08a9a237992ac19b7b1c
PreziosiR <rpreziosi00@gmail.com>
2021-12-22 16:53:14 +0100

    appbar customization

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,20 @@ class Core extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      title: "Flutter Shop",
+      home: Scaffold(
+        appBar: AppBar(backgroundColor: const Color(0xFFFFFFFF),
+          leading: IconButton(onPressed: () {}, icon: const Icon(Icons.arrow_back), color: const Color(0xFF000000)),
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.search), color: const Color(0xFF000000)),
+            IconButton(onPressed: () {}, icon: const Icon(Icons.shopping_cart), color: const Color(0xFF000000))
+          ],
+        ),
+        body: const Center(
+          child: Text("Hello"),
+        ),
+      ),
     );
   }
 }
