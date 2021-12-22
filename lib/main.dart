2f4637c5c63725c6edb41093c3ec740da84af10a
PreziosiR <rpreziosi00@gmail.com>
2021-12-22 13:29:24 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Core());
+}
+
+class Core extends StatelessWidget {
+  const Core({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
