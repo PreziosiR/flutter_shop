4dffef570e863eb216e5843f773b34fcb02e7577
PreziosiR <rpreziosi00@gmail.com>
2021-12-26 17:03:58 +0100

    add products data

--- lib/screens/home/homeScreen.dart
+++ lib/screens/home/homeScreen.dart
@@ -17,10 +17,11 @@ class HomeScreen extends StatelessWidget {
 AppBar buildAppBar() {
   return AppBar(
           backgroundColor: Colors.white,
-          leading: IconButton(onPressed: () {}, icon: const Icon(Icons.arrow_back), color: const Color(0xFF000000)),
+          elevation: 0.0,
+          leading: IconButton(onPressed: () {}, icon: const Icon(Icons.arrow_back), color: Colors.black),
           actions: [
-            IconButton(onPressed: () {}, icon: const Icon(Icons.search), color: const Color(0xFF000000)),
-            IconButton(onPressed: () {}, icon: const Icon(Icons.shopping_cart), color: const Color(0xFF000000))
+            IconButton(onPressed: () {}, icon: const Icon(Icons.search), color: Colors.black),
+            IconButton(onPressed: () {}, icon: const Icon(Icons.shopping_cart), color: Colors.black)
           ],
         );
 }
